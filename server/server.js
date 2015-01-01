6f4fa4f61995078b88ccfa4e3dde302595547367
ezzye <ezzy.elliott@gmail.com>
2015-01-01 19:06:41 +0000

    add use to call static

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ var Post = require('./models/post')
 var app = express()
 app.use(bodyParser.json())
 app.use('/api/posts',require('./controllers/api/posts'))
-app.use(require('./controllers/static'))
-// equivalent to: app.use('/', require('./controllers/static')
+// app.use(require('./controllers/static'))
+app.use('/', require('./controllers/static'))
 
 app.listen(3000, function () {
     console.log('Server listening on', 3000)
