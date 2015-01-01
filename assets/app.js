9c46b5186c6ee66f9ff87bb6c90426a2fda64802
ezzye <ezzy.elliott@gmail.com>
2015-01-01 16:39:36 +0000

    moved assets to separate file js controller

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,18 @@
-text
+var app = angular.module('app', [])
+app.controller('PostCtrl', function($scope, $http) {
+    $scope.addPost = function() {
+        if ($scope.postBody) {
+            $http.post('/api/posts', {
+                username: 'dickeyxxx',
+                body: $scope.postBody
+            }).success(function(post) {
+                $scope.posts.unshift(post)
+                $scope.postBody = null
+            })
+        }
+    }
+    $http.get('/api/posts')
+        .success(function(posts) {
+        $scope.posts = posts
+    })
+})
