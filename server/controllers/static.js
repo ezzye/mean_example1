fd5731e16667c93ee1792060fbbbed54ed0bcfd9
ezzye <ezzy.elliott@gmail.com>
2015-01-01 19:28:31 +0000

    add sendfile post.html

--- server/controllers/static.js
+++ server/controllers/static.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var router = express.Router()
 
+router.get('/', function (req, res) {
+    res.sendfile('layouts/posts.htm')
+})
+
 router.use(express.static(__dirname + '/../assets'))
 
 module.exports = router
