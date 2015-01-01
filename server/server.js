429b580df6f07fa1d998c1561bfe07ce99c2ab70
ezzye <ezzy.elliott@gmail.com>
2015-01-01 01:07:05 +0000

    sort entires by dates

--- server/server.js
+++ server/server.js
@@ -6,8 +6,11 @@ var app = express()
 app.use(bodyParser.json())
 
 app.get('/api/posts', function (req, res, next) {
-    Post.find(function(err, posts) {
-        if (err) { return next(err) }
+    Post.find()
+    .sort('-date')
+    .exec(function(err, posts) {
+        if (err) { return next(err) 
+        }
         res.json(posts)
     })
 })
