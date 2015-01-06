4b8ca50272055fdd28a830aa263ceaa447359936
ezzye <ezzy.elliott@gmail.com>
2015-01-06 20:56:50 +0000

    Only requires in gulp

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,9 +1,4 @@
-gulp.task('watch:js', ['js'], function () {
-    gulp.watch('ng/**/*.js', ['js'])
-})
-
 var fs = require('fs')
 fs.readdirSync(__dirname + '/gulp').forEach(function(task) {
     require('./gulp/' + task)
 });
-
