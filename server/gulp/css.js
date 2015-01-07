230a28bc55b8081fcd729cf34f96516c1b98ee43
ezzye <ezzy.elliott@gmail.com>
2015-01-07 20:53:34 +0000

    changed spelling to src from scr omg

--- server/gulp/css.js
+++ server/gulp/css.js
@@ -2,7 +2,7 @@ var gulp = require('gulp')
 var stylus = require('gulp-stylus')
 
 gulp.task('css', function () {
-    gulp.scr('css/**/*.styl')
+    gulp.src('css/**/*.styl')
     .pipe(stylus())
     .pipe(gulp.dest('assets'))
 })
