4fec6c73d8c72c05f9c29855e3b392e0530c9426
ezzye <ezzy.elliott@gmail.com>
2015-01-08 13:06:48 +0000

    add gulp dev task

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -2,3 +2,6 @@ var fs = require('fs')
 fs.readdirSync(__dirname + '/gulp').forEach(function(task) {
     require('./gulp/' + task)
 });
+
+var gulp =require('gulp')
+gulp.task('dev', ['watch:css', 'watch:js'])
