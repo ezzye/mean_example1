abac189b210843af1660a743bbce23c6ab3ee1b8
ezzye <ezzy.elliott@gmail.com>
2015-01-13 16:52:23 +0000

    add callback to validte password bcrypt hash

--- authapp/server.js
+++ authapp/server.js
@@ -1,28 +1,30 @@
 var express = require('express')
 var jwt = require('jwt-simple')
+var bcrypt = require('bcryptjs')
 var _ = require('lodash')
 var app = express()
 app.use(require('body-parser').json())
 
-var users = [{username:'dickeyxxx', password: 'pass'}]
+var users = [{username:'dickeyxxx', password: '$2a$10$Jmo/n32ofSM9JvzfH0z6Me6TMyn6C/U9JhzDG8xhQC4ExHMG1jXz2'}]
 var secretKey = 'supersecretkey'
 
 function findUserByUsername(username) {
     return _.find(users, {username: username})
 }
 
-function validateUser (user,password) {
-    return user.password === password
+function validateUser (user,password, cb) {
+    bcrypt.compare(password, user.password, cb)
 }
 
 app.post('/session', function (req, res) {
     var user = findUserByUsername(req.body.username)
-    if (!validateUser(user, req.body.password)) {
-        return res.send(401) // Unauthorised
-    }
-
-    var token = jwt.encode({username: user.username}, secretKey)
-    res.json(token)
+    validateUser(user, req.body.password, function (err, valid) {
+        if (err || !valid) {
+            return res.send(401)
+        }
+        var token = jwt.encode({username: user.username}, secretKey)
+        res.json(token)
+    })
 })
 
 app.get('/user', function (req, res) {
