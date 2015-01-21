ea8b4988db7517dd1145b9ec45da43a5e2270719
ezzye <ezzy.elliott@gmail.com>
2015-01-21 21:14:15 +0000

    changed sendFile path to absolute

--- server/controllers/static.js
+++ server/controllers/static.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function (req, res) {
-    res.sendFile('layouts/app.html')
+    res.sendFile(__dirname+'layouts/app.html')
 })
 
 router.use(express.static(__dirname + '/../assets'))
