0565b6869547d51039f3affaa6b5230769e7ae93
ezzye <ezzy.elliott@gmail.com>
2015-01-21 21:36:49 +0000

    Comments removed

--- server/ng/login.ctrl.js
+++ server/ng/login.ctrl.js
@@ -1,12 +1,9 @@
 angular.module('app')
 .controller('LoginCtrl', function ($scope, UserSvc) {
-    console.log('login service called vvvvvvvvvvvvvvvvvvvvvvvvv')
     $scope.login = function (username, password) {
-        console.log('scope.login function called wwwwwwwwwwwwwwwwwwwwwwwww')
         UserSvc.login(username, password)
         .then(function (user) {
             console.log(user)
-            console.log('Password found in login.ctrl.js')
         })
     }
 })
