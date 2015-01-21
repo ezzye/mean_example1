27e07c415826dcddc278b847e6d3d111cbc340e0
ezzye <ezzy.elliott@gmail.com>
2015-01-21 21:26:36 +0000

    Add another log comment

--- server/ng/login.ctrl.js
+++ server/ng/login.ctrl.js
@@ -2,6 +2,7 @@ angular.module('app')
 .controller('LoginCtrl', function ($scope, UserSvc) {
     console.log('login service called vvvvvvvvvvvvvvvvvvvvvvvvv')
     $scope.login = function (username, password) {
+        console.log('scope.login function called wwwwwwwwwwwwwwwwwwwwwwwww')
         UserSvc.login(username, password)
         .then(function (user) {
             console.log(user)
