9180f031653f7199c6eca7302956e698f3713c4a
ezzye <ezzy.elliott@gmail.com>
2015-01-24 16:44:39 +0000

    add auth.js to server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ var bodyParser = require('body-parser')
 var Post = require('./models/post')
 
 var app = express()
+
+app.use(require('./auth'))
 app.use(bodyParser.json())
 app.use('/api/posts',require('./controllers/api/posts'))
 // app.use(require('./controllers/static'))
