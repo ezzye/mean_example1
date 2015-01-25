e8b40945b24ec1b59b31b353343b40f31f7002af
ezzye <ezzy.elliott@gmail.com>
2015-01-25 19:26:43 +0000

    remove header

--- server/ng/user.svc.js
+++ server/ng/user.svc.js
@@ -19,8 +19,6 @@ angular.module('app')
             username: username,
             password: password
         }).then(function (val) {
-           svc.token = val.data
-           $http.defaults.headers.common['X-Auth'] = val.data
            return svc.getUser()
         })
     }
