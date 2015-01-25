a57eb3250e9690066c52144bdea8218019cc8b11
ezzye <ezzy.elliott@gmail.com>
2015-01-25 19:23:34 +0000

    Add header to user

--- server/ng/user.svc.js
+++ server/ng/user.svc.js
@@ -18,8 +18,10 @@ angular.module('app')
         return $http.post('/api/users', {
             username: username,
             password: password
-        }).then(function () {
-            return svc.getUser()
+        }).then(function (val) {
+           svc.token = val.data
+           $http.defaults.headers.common['X-Auth'] = val.data
+           return svc.getUser()
         })
     }
 })
