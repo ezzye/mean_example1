86aeb9e3fc973d630e6a6e4e5f26528de313d029
ezzye <ezzy.elliott@gmail.com>
2015-01-25 20:49:02 +0000

    Remove login

--- server/ng/register.ctrl.js
+++ server/ng/register.ctrl.js
@@ -2,6 +2,5 @@ angular.module('app')
 .controller('RegisterCtrl', function ($scope, UserSvc) {
     $scope.register = function (username, password) {
         UserSvc.register(username, password)
-        UserSvc.login(username, password)
     }
 })
