7a42db3755e09669e3bc7b168bf193cfbf6b3b56
ezzye <ezzy.elliott@gmail.com>
2015-01-26 14:15:38 +0000

    Add emit login response

--- server/ng/register.ctrl.js
+++ server/ng/register.ctrl.js
@@ -2,5 +2,8 @@ angular.module('app')
 .controller('RegisterCtrl', function ($scope, UserSvc) {
     $scope.register = function (username, password) {
         UserSvc.register(username, password)
+        .then(function (response) {
+        $scope.$emit('login', response.data)
+        })
     }
 })
