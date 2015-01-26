3e5526dcc8e1b2978bdb615a2f573c88a38482b6
ezzye <ezzy.elliott@gmail.com>
2015-01-26 14:47:06 +0000

    Add location as a parameter

--- server/ng/login.ctrl.js
+++ server/ng/login.ctrl.js
@@ -1,5 +1,5 @@
 angular.module('app')
-.controller('LoginCtrl', function ($scope, UserSvc) {
+.controller('LoginCtrl', function ($scope, UserSvc, $location) {
     $scope.login = function (username, password) {
         UserSvc.login(username, password)
         .then(function (response) {
