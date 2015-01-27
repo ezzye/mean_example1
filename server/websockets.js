e91ab0314a670c13a1e35cdaac47a5e89b315327
ezzye <ezzy.elliott@gmail.com>
2015-01-27 17:45:36 +0000

    testing some changes

--- server/websockets.js
+++ server/websockets.js
@@ -2,11 +2,9 @@ var ws = require('ws')
 exports.connect = function (server) {
     var wss = new ws.Server({server: server})
     wss.on('connection', function (ws) {
-        console.log('connection made')
-        ws.send('hello');
         wss.on('message', function (message) {
-            console.log('received: %s', message)
-            ws.send(message);
+            console.log('received: %s', message);
+            ws.send('something');
         });
     });
 }
