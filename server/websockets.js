c8679fc22a3ab1b039633148df481aea4b3017fe
ezzye <ezzy.elliott@gmail.com>
2015-01-27 17:38:41 +0000

    Move message into connection block

--- server/websockets.js
+++ server/websockets.js
@@ -2,10 +2,11 @@ var ws = require('ws')
 exports.connect = function (server) {
     var wss = new ws.Server({server: server})
     wss.on('connection', function (ws) {
+        console.log('connection made')
         ws.send('hello');
-    });
-    wss.on('message', function (message) {
-        console.log('received: %s', message)
-        ws.send(message);
+        wss.on('message', function (message) {
+            console.log('received: %s', message)
+            ws.send(message);
+        });
     });
 }
