6983382de73d0c0ba018e1cffe2b9b7f68c1b26c
ezzye <ezzy.elliott@gmail.com>
2015-01-27 17:54:16 +0000

    Back to test code

--- server/websockets.js
+++ server/websockets.js
@@ -2,9 +2,6 @@ var ws = require('ws')
 exports.connect = function (server) {
     var wss = new ws.Server({server: server})
     wss.on('connection', function (ws) {
-        wss.on('message', function (message) {
-            console.log('received: %s', message);
-            ws.send('something');
-        });
-    });
+        ws.send('something')
+    })
 }
