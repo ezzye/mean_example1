5febc6728b405d0de1585e393223a2b89e88315c
ezzye <ezzy.elliott@gmail.com>
2015-01-28 07:52:10 +0000

    Add connection t clients array

--- server/websockets.js
+++ server/websockets.js
@@ -1,7 +1,15 @@
+var _ = require ('lodash')
 var ws = require('ws')
+var clients =[]
+
 exports.connect = function (server) {
     var wss = new ws.Server({server: server})
     wss.on('connection', function (ws) {
-        ws.send('something')
+        clients.push(ws)
+        console.log('client added %s', clients)
+        ws.on('close', function () {
+            _.remove(clients, ws)
+            console.console.log('client removed %s', clients)
+        })
     })
 }
