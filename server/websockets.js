6ee31a83855adf7ed64e5e8989ea0379c11aa012
ezzye <ezzy.elliott@gmail.com>
2015-01-29 08:04:07 +0000

    Add broadcast function

--- server/websockets.js
+++ server/websockets.js
@@ -6,10 +6,18 @@ exports.connect = function (server) {
     var wss = new ws.Server({server: server})
     wss.on('connection', function (ws) {
         clients.push(ws)
+        exports.broadcast('new client joined')
         console.log('client added %s', clients)
         ws.on('close', function () {
             _.remove(clients, ws)
             console.log('client removed %s', clients)
         })
+    })
+}
+
+exports.broadcast = function (topic, data) {
+    var json = JSON.stringify({topic: topic, data: data})
+    clients.forEach(function (client) {
+        client.send(json)
     })
 }
