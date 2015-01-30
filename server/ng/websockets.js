60b6fdc8683fe7fec0c8690bbbc6eedb5a52fdd0
ezzye <ezzy.elliott@gmail.com>
2015-01-30 18:51:14 +0000

    Added check for websocket closed

--- server/ng/websockets.js
+++ server/ng/websockets.js
@@ -1,15 +1,21 @@
 angular.module('app')
-.run(function ($rootScope) {
-    var url = 'ws://localhost:3000'
-    var connection = new WebSocket (url)
-    
-    connection.onopen = function () {
-        console.log('WebSocket connected')
-    }
-    
-    connection.onmessage = function (e) {
-        console.log(e)
-        var payload = JSON.parse(e.data)
-        $rootScope.$broadcast('ws:' + payload.topic, payload.data)
-    }
+.run(function ($rootScope, $timeout) {
+    (function connect() {
+        var url = 'ws://localhost:3000'
+        var connection = new WebSocket (url)
+        
+        connection.onclose = function (e) {
+            console.log('WebSocket closed.  Reconnecting...')
+            $timeout(connect, 10*1000)
+        }
+        connection.onmessage = function (e) {
+            console.log(e)
+            var payload = JSON.parse(e.data)
+            $rootScope.$broadcast('ws:' + payload.topic, payload.data)
+        }
+        connection.onopen = function () {
+            console.log('WebSocket connected')
+        }
+        
+    }) ()
 })
