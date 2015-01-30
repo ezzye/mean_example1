94f80b5da7d1ba6c2c74ed1e05fdc50002a6c9d0
ezzye <ezzy.elliott@gmail.com>
2015-01-30 12:39:08 +0000

    Add websocket post to post document

--- server/ng/posts.ctrl.js
+++ server/ng/posts.ctrl.js
@@ -16,4 +16,10 @@ angular.module('app')
     PostsSvc.fetch().success(function (posts) {
         $scope.posts = posts
     })
+    
+    $scope.$on('ws:new_post', function (_, post) {
+        $scope.$apply(function () {
+            $scope.posts.unshift(post)
+        })
+    })
 })
