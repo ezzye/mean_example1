08dd061117e1e5f2d5628117371556b17ef63ee7
ezzye <ezzy.elliott@gmail.com>
2015-01-30 17:44:06 +0000

    Remodelled post controller

--- server/ng/posts.ctrl.js
+++ server/ng/posts.ctrl.js
@@ -3,23 +3,23 @@ angular.module('app')
     $scope.addPost = function () {
         if($scope.postBody) {
             PostsSvc.create({
-                username: 'dickeyxxx',
                 body: $scope.postBody
-            }).success(function (post)
+            }).then(function ()
             {
-                $scope.posts.unshift(post)
                 $scope.postBody=null
             })
         }
     }
 
-    PostsSvc.fetch().success(function (posts) {
-        $scope.posts = posts
-    })
-    
     $scope.$on('ws:new_post', function (_, post) {
         $scope.$apply(function () {
             $scope.posts.unshift(post)
         })
     })
+
+    PostsSvc.fetch().success(function (posts) {
+        $scope.posts = posts
+    })
+    
+
 })
