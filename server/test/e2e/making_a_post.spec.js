30c5e752e1c5d37d4c14e84b7386a5cca5b5b4a1
ezzye <ezzy.elliott@gmail.com>
2015-02-02 08:14:28 +0000

    Added click login test

--- server/test/e2e/making_a_post.spec.js
+++ server/test/e2e/making_a_post.spec.js
@@ -1,7 +1,7 @@
 describe('making a post', function () {
     it('logs in and creates a new post', function () {
         browser.get('http://localhost:3001')
-        // click 'login'
+        element(by.css('nav .login')).click()
         // fill out and submit login form
         // submit a new post on the posts page
         // the user should now see their post as the first post on the page
