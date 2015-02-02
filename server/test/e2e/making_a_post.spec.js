8c2e0178e12ab2c6f836bcd45bc7cabdce9c36b8
ezzye <ezzy.elliott@gmail.com>
2015-02-02 17:26:55 +0000

    removed pause

--- server/test/e2e/making_a_post.spec.js
+++ server/test/e2e/making_a_post.spec.js
@@ -3,16 +3,14 @@ describe('making a post', function () {
         browser.get('http://localhost:3001')
         // click 'login'
         element(by.css('nav .login')).click()
-        browser.pause()
         // fill out and submit login form
         element(by.model('username')).sendKeys('dickeyxxx')
         element(by.model('password')).sendKeys('pass')
         element(by.css('form .btn')).click()
-        browser.pause()
+
         // submit a new post on the posts page
         var post = 'my new psot'
         element(by.model('postBody')).sendKeys(post)
-        browser.pause()
         element(by.css('form .btn')).click()
         
         // the user should now see their post as the first post on the page
