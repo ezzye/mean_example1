d752e4096b8642e5f2414deef65136a7a8aa85c7
ezzye <ezzy.elliott@gmail.com>
2015-02-02 12:50:32 +0000

    Add test for test making a posting

--- server/test/e2e/making_a_post.spec.js
+++ server/test/e2e/making_a_post.spec.js
@@ -1,10 +1,19 @@
 describe('making a post', function () {
     it('logs in and creates a new post', function () {
         browser.get('http://localhost:3001')
+        // click 'login'
         element(by.css('nav .login')).click()
-        browser.pause()
+        
         // fill out and submit login form
+        element(by.model('username')).sendKeys('dickeyxxx')
+        element(by.model('password')).sendKeys('pass')
+        element(by.css('form .btn')).click()
+        
         // submit a new post on the posts page
+        var post = 'my new psot'
+        element(by.model('postBody')).sendKeys(post)
+        elelment(by.css('form .btn')).click()
+        
         // the user should now see their post as the first post on the page
     })
 })
