8a61779d9af227a66156c4354060e448b04653d5
ezzye <ezzy.elliott@gmail.com>
2015-02-02 08:17:09 +0000

    add browser pause

--- server/test/e2e/making_a_post.spec.js
+++ server/test/e2e/making_a_post.spec.js
@@ -2,6 +2,7 @@ describe('making a post', function () {
     it('logs in and creates a new post', function () {
         browser.get('http://localhost:3001')
         element(by.css('nav .login')).click()
+        browser.pause()
         // fill out and submit login form
         // submit a new post on the posts page
         // the user should now see their post as the first post on the page
