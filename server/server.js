03230361e64376ff70c4ee4b860af9aad11c2f9a
ezzye <ezzy.elliott@gmail.com>
2015-02-05 17:41:50 +0000

    Removed parts not used

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
 var express = require('express')
-var bodyParser = require('body-parser')
-var Post = require('./models/post')
 var logger = require('morgan')
 var websockets = require('./websockets')
-
 var app = express()
+
+app.use(logger('dev'))
 
 
 app.use(require('./controllers'))
 
 var port = process.env.PORT || 3000
-
 var server = app.listen(port, function () {
     console.log('Server', process.pid, 'listening on', port)
 })
