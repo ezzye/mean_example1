686cca08503546e02f3cab31383aa691f3241934
ezzye <ezzy.elliott@gmail.com>
2015-02-05 21:36:46 +0000

    test of post

--- server/test/server/controllers/api/posts.spec.js
+++ server/test/server/controllers/api/posts.spec.js
@@ -1,8 +1,13 @@
 var expect = require('chai').expect
-var ctrl = require('../../../../controllers/api/posts')
+
+var api = require('../../support/api')
 
 describe('controllers.api.posts', function () {
-    it('exists', function () {
-        expect (ctrl).to.exist
+    describe('GET /api/posts', function () {
+        it('exists', function (done) {
+            api.get('/api/posts')
+            .expect(200)
+            .end(done)
+        })
     })
 })
