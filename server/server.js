50e603115293cbbb9e8a7ce0f5a0e05d8fd0168b
ezzye <ezzy.elliott@gmail.com>
2015-02-05 17:33:35 +0000

    Moved router stuff to index file

--- server/server.js
+++ server/server.js
@@ -6,15 +6,8 @@ var websockets = require('./websockets')
 
 var app = express()
 
-app.use(require('./auth'))
-app.use(bodyParser.json())
 
-app.use('/api/posts',require('./controllers/api/posts'))
-// app.use(require('./controllers/static'))
-app.use('/', require('./controllers/static'))
-
-app.use('/api/sessions', require('./controllers/api/sessions'))
-app.use('/api/users', require('./controllers/api/users'))
+app.use(require('./controllers'))
 
 var port = process.env.PORT || 3000
 
